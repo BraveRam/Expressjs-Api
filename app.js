d039bf70f5bd41a72c8fbc9248edaebd860e901f
BraveRam <99811385+BraveRam@users.noreply.github.com>
2023-12-21 12:48:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const cors = require("cors");
 const app = express();
 const PORT = 8000;
 
-app.use(cors());
+app.use(cors({
+ origin: ['https://www.section.io', 'https://www.google.com/']
+ }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
